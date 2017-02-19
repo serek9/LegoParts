47c734fdd50623d71325b64d9d4d35ed1f0fd3e0
sergiodiaz <sergiodiaz808@gmail.com>
2017-02-19 14:27:41 +0100

    Finished.

--- app/src/main/java/com/example/dam/legoparts/MainActivity.java
+++ app/src/main/java/com/example/dam/legoparts/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     protected EditText setText;
     protected Button goButton;
+    protected LinearLayout exemple1;
+    protected LinearLayout exemple2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setText = (EditText) findViewById(R.id.editText1);
         goButton = (Button) findViewById(R.id.button1);
-        setText.setText("");
+        exemple1 = (LinearLayout) findViewById(R.id.example1);
+        exemple2 = (LinearLayout) findViewById(R.id.example2);
+
+        exemple1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setText.setText("7065-1");
+            }
+        });
+
+        exemple2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setText.setText("21125-1");
+            }
+        });
+
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
